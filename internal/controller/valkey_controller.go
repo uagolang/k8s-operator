102b64be226436d84133d37818fe149d5982476a
nesymno <nesymno@gmail.com>
2025-02-24 12:58:59 +0700

    add operator access to v1 api group resources

--- internal/controller/valkey_controller.go
+++ internal/controller/valkey_controller.go
@@ -43,6 +43,7 @@ type ValkeyReconciler struct {
 //+kubebuilder:rbac:groups=database.kuberly.io,resources=valkeys,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=database.kuberly.io,resources=valkeys/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=database.kuberly.io,resources=valkeys/finalizers,verbs=update
+//+kubebuilder:rbac:groups=v1,resources=*,verbs=*
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
